1b5e9d81162f0513a79c34b6c9a27d4b3e95f9ab
ozerbaykal <baykalozer87@gmail.com>
2024-10-26 02:40:33 +0300

    pages add,routing is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Main from "./pages/Main"
+import Create from "./pages/Create"
+import Detail from "./pages/Detail"
+import Edit from "./pages/Edit"
+
+
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/new" element={<Create />} />
+        <Route path="/note/:id" element={<Detail />} />
+        <Route path="/note/:id/edit" element={<Edit />} />
+
+
+
+
+
+
+
+      </Routes>
+
+
+    </BrowserRouter>
+
   )
 }
 
