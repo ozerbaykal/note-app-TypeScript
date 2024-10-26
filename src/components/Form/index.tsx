4afdfefe0e514f0ae27e91037cd983b619e41e89
ozerbaykal <baykalozer87@gmail.com>
2024-10-26 04:07:09 +0300

    customForm

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,15 +1,44 @@
-import { Col, Form, Row } from "react-bootstrap"
+import { FormEvent, useRef, useState } from "react"
+import { Button, Col, Form, Row, Stack } from "react-bootstrap"
+import { Link } from "react-router-dom"
+import ReactSelect from "react-select/creatable"
+import { Tag } from "../../types.ts"
 
 const CustomForm = () => {
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    const textRef = useRef<HTMLTextAreaElement>(null);
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+
+
+    //form gönderilince çalışır
+    const handleSubmit = (e: FormEvent) => {
+        e.preventDefault();
+
+        const title = inputRef.current?.value;
+        const markdown = textRef.current?.value;
+        console.log(title, markdown)
+
+
+
+
+
+
+
+
+    }
+
     return (
 
-        <Form className="my-5">
+        <Form
+            onSubmit={handleSubmit}
+            className="my-5">
             {/* başlık etiket inputu */}
             <Row>
                 <Col>
                     <Form.Group>
                         <Form.Label>Başlık</Form.Label>
-                        <Form.Control />
+                        <Form.Control ref={inputRef} />
                     </Form.Group>
 
 
@@ -20,7 +49,7 @@ const CustomForm = () => {
                 <Col>
                     <Form.Group>
                         <Form.Label>Etiketler</Form.Label>
-                        <Form.Control />
+                        <ReactSelect />
                     </Form.Group>
                 </Col>
 
@@ -29,10 +58,22 @@ const CustomForm = () => {
             {/* içerik  */}
             <Form.Group className="mt-4">
                 <Form.Label>içerik (markdown destekler)</Form.Label>
-                <Form.Control as="textarea" style={{ minHeight: "300px", maxHeight: "500px" }} />
+                <Form.Control ref={textRef} as="textarea" style={{ minHeight: "300px", maxHeight: "500px" }} />
 
 
             </Form.Group>
+
+            {/* butonlar */}
+            <Stack direction="horizontal" className="justify-content-end mt-5" gap={4}>
+                <Link to={".."} >
+                    <Button type="button" variant="secondary">Geri</Button>
+                </Link>
+                <Button type="submit">Kaydet</Button>
+
+
+
+            </Stack>
+
 
         </Form>
     )
