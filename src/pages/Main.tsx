e7c3dc81486f5a801bc0e691c5fe78fb081cdc33
ozerbaykal <baykalozer87@gmail.com>
2024-11-01 12:00:52 +0300

    mainPage filtred

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,12 +1,35 @@
-import { Button, Col, Container, Form, FormGroup, Row, Stack } from "react-bootstrap";
+import { Button, Col, Container, Form, Row, Stack } from "react-bootstrap";
 import { Note, Tag } from "../types";
 import { Link } from "react-router-dom";
+import Card from "../components/Card";
+import ReactSelect from "react-select";
+import { useMemo, useState } from "react";
+
 
 type Props = {
     availableTags: Tag[];
     notes: Note[];
 };
 const Main = ({ availableTags, notes }: Props) => {
+    const [query, setQuery] = useState<string>("")
+    const [selectedTags, setSelectedTags] = useState<Tag[]>([])
+
+    /*
+    1) Not başlığı 1.inputta aratılan metni içermelidir.Note 'un başlığının küçük harfle çevrilmiş hali aratılan metnin küçük hale çevrilmiş halini içeriyorsa koşul sağlanır
+   
+
+    &&
+    2) Input ile seçilen etiketler note 'un içerisindeki etiketler ile birebir eşleşmelidir.Seçilen etiketler id'nizdeki her bir etiket için note'a ait etiketler arasında eşleşme kontrolü yapıcaz
+    
+     */
+
+
+    const filtredNotes = useMemo(() =>
+        notes.filter((note) =>
+            note.title.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+            &&
+            selectedTags.every((s_tag) => note.tags.some((note_tag) => note_tag.value === s_tag.value))
+        ), [query, selectedTags])
 
     return (
         <Container className="mx-auto my-5">
@@ -28,7 +51,7 @@ const Main = ({ availableTags, notes }: Props) => {
                     <Col>
                         <Form.Group>
                             <Form.Label>Başlığa Göre Ara</Form.Label>
-                            <Form.Control />
+                            <Form.Control onChange={(e) => setQuery(e.target.value)} />
                         </Form.Group>
                     </Col>
 
@@ -36,7 +59,11 @@ const Main = ({ availableTags, notes }: Props) => {
                     <Col>
                         <Form.Group>
                             <Form.Label> Etikete Göre Ara</Form.Label>
-                            <Form.Control />
+                            <ReactSelect
+                                onChange={(all_tags) => setSelectedTags(all_tags as Tag[])}
+                                options={availableTags}
+                                isMulti
+                                className="text-black" />
                         </Form.Group>
                     </Col>
 
@@ -50,11 +77,16 @@ const Main = ({ availableTags, notes }: Props) => {
 
             {/* Not Listesi */}
 
-            {notes.map((note) => (
+            <Row xs={1} sm={2} lg={3} xl={4} className="mt-4 g-4">
+                {filtredNotes.map((note) => (
 
-                <div>Note</div>
+                    <Col key={note.id}>
 
-            ))}
+                        <Card note={note} />
+                    </Col>
+
+                ))}
+            </Row>
 
 
 
